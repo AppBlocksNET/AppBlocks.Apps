fe36a1f59f4cf877429527a171890349d613c9d6
David Walker <dave@radicaldave.com>
2021-05-15 11:32:56 -0500

    init

---
+++ src/AppBlocks.Apps/IApp.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AppBlocks.Apps
+{
+    public interface IApp
+    {
+        List<Dictionary<string, object>> App(Dictionary<string, object> parameters = null);
+    }
+}
